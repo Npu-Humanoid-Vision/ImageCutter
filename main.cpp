35abf88a0867921f0822092fbc6f2a785ca7e0e7
Alex_Beng <599267065@qq.com>
2019-01-17 09:08:31 +0800

    init commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "HogGetter.h"
+#include <bits/stdc++.h>
+#include <opencv2/opencv.hpp>
+using namespace std;
+using namespace cv;
+
+int main(int argc, char const *argv[]) {
+    
+    return 0;
+}
